428683ab50b467a70c8d26e6f605c9ced580549f
Marshall Perrin <mperrin@stsci.edu>
2015-04-22 01:26:54 -0400

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -39,8 +39,8 @@ source_encoding = 'iso-8859-15'
 master_doc = 'index'
 
 # General information about the project.
-project = u'GPI Python data analysis tools'
-copyright = u'2010-2014, GPI Data Analysis Team'
+project = u'GPI PSF Simulator'
+copyright = u'2015, GPI Data Analysis Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -165,39 +165,6 @@ html_last_updated_fmt = '%b %d, %Y'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'gpi_installdoc'
-
-
-# -- Options for LaTeX output --------------------------------------------------
-
-# The paper size ('letter' or 'a4').
-latex_paper_size = 'letter'
-
-# The font size ('10pt', '11pt' or '12pt').
-#latex_font_size = '10pt'
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-  ('index', 'gpidata.tex', u'GPI Data Pipeline',
-   u'Marshall Perrin, Jerome Maire, and the GPI data analysis team', 'manual'),
-]
-
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#latex_logo = None
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-latex_use_parts = True
-
-# Additional stuff for the LaTeX preamble.
-#latex_preamble = ''
-
-# Documents to append as an appendix to all manuals.
-#latex_appendices = []
-
-# If false, no module index is generated.
-#latex_use_modindex = True
 
 
 # -- Options for autodoc ----------------------------
--- docs/usage.rst
+++ docs/usage.rst
@@ -1,3 +1,24 @@
 
 Usage Instructions will Eventually Go Here
 ============================================
+
+
+For now here is a basic example::
+
+
+    >>> import gpipsfs
+    >>> gpi = gpipsfs.GPI()
+    >>> gpi.obsmode = "J_coron"
+    >>> psf = gpi.calcPSF(display=True)
+    >>> psf.writeto('my_J_psf.fits')
+
+
+The GPI class has attributes for the filter, apodizer, occulter, and Lyot mask.
+You can set them indivdually, or just set the obsmode to control the entire set
+at once in the same manner as the actual instrument. 
+
+
+Results are returned as FITS files. 
+
+See the documentation for POPPY or WebbPSF for more general background on how
+this code works. 
